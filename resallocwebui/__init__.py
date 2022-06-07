09b14e0de839aba9e67da7e40960a2ba7c383ead
Jakub Kadlcik <frostyx@email.cz>
2022-06-07 08:38:37 -0400

    webui: if possible, use static files and templates from this directory
    In case we are working within the upstream project, we want to use static
    files and templates from git (for easier development). Otherwise we want to
    use files installed from the RPM package.

--- resallocwebui/__init__.py
+++ resallocwebui/__init__.py
@@ -1,1 +1,17 @@
-static_folder = "/usr/share/resallocwebui"
+import os
+import sys
+
+
+# In case we are working within the upstream project, we want to use static
+# files and templates from git (for easier development). Otherwise we want to
+# use files installed from the RPM package.
+here = os.path.abspath(os.path.dirname(__file__))
+datadirs = [here, "/usr/share/resallocwebui"]
+for datadir in datadirs:
+    staticdir = os.path.join(datadir, "static")
+    templatedir = os.path.join(datadir, "templates")
+    if os.path.isdir(staticdir) and os.path.isdir(templatedir):
+        break
+else:
+    print("Error: Not found any valid data directory")
+    sys.exit(1)
